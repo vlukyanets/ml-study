701b96ef1ceb20c99596da79798ce6f257a2a42f
Valentin Lukyanets <valikluks95@gmail.com>
2020-08-20 10:54:17 +0300

    Modified existing NN for digits recognition
    - Modified code to be generic and independent from some of NN parameters
    - Moved common code to separate function
    - Added CLI parameter: iterations

--- digits_recognition.py
+++ digits_recognition.py
@@ -2,14 +2,19 @@ from sklearn import datasets as sklearndatasets
 import numpy
 import random
 import matplotlib.pyplot as plt
+import argparse
 
 
-def sigmoid(x):
-    if x > 30:
+def sigmoid(x: float):
+    if x > 50:
         return 1.0
-    elif x < -30:
+    elif x < -50:
         return 0.0
     return 1.0 / (1.0 + numpy.exp(-x))
+
+
+def sigmoid_array(x):
+    return numpy.asarray([sigmoid(z) for z in x])
 
 
 def derivative(x):
@@ -27,33 +32,32 @@ def bounds(x, left, right):
 class DigitsRecognitionNeuralNetwork:
     def __init__(self):
         # Layers: 64 -> 32 -> 20 -> 10
-        self.layer_sizes = [(64, 32), (32, 20), (20, 10)]
-        self.weights = [2 * numpy.random.random_sample(layer_size) - 1
-                        for layer_size in self.layer_sizes]
+        self.layer_sizes = [64, 32, 20, 10]
+        self.interlayer_sizes = [(x, y) for x, y in zip(self.layer_sizes[:-1], self.layer_sizes[1:])]
+        self.depth = len(self.layer_sizes)
+        self.weights = [2 * numpy.random.random_sample(interlayer_size) - 1
+                        for interlayer_size in self.interlayer_sizes]
+
+    def calculate_values_and_errors_output_layer(self, image, target):
+        values = [numpy.divide(numpy.copy(image).flatten(), 15.0)]
+        for i, _ in enumerate(self.layer_sizes[1:]):
+            values.append(sigmoid_array(numpy.dot(values[i], self.weights[i])))
+
+        errors_output_layer = -values[-1]
+        errors_output_layer[target] += 1.0
+        return values, errors_output_layer
 
     def train_sample(self, data, learning_coef):
         (image, target) = data
-        values = [numpy.divide(numpy.copy(image).flatten(), 15.0),
-                  numpy.zeros(32),
-                  numpy.zeros(20),
-                  numpy.zeros(10)]
+        values, errors_output_layer = self.calculate_values_and_errors_output_layer(image, target)
 
-        for i in range(3):
-            values[i+1] = numpy.dot(values[i], self.weights[i])
-            values[i+1] = numpy.asarray([sigmoid(x) for x in values[i+1]])
+        errors = [numpy.zeros(size) for size in self.layer_sizes[:-1]]
+        errors.append(errors_output_layer)
 
-        errors = [numpy.zeros(64),
-                  numpy.zeros(32),
-                  numpy.zeros(20),
-                  numpy.zeros(10)]
-        errors[3] = -values[3]
-        errors[3][target] += 1
-
-        for i in range(2, -1, -1):
+        for i in range(self.depth-2, -1, -1):
             errors[i] = numpy.dot(errors[i+1], numpy.transpose(self.weights[i]))
 
-        for i in range(3):
-            n, m = self.layer_sizes[i]
+        for i, (n, m) in enumerate(self.interlayer_sizes):
             for x in range(n):
                 for y in range(m):
                     self.weights[i][x][y] += learning_coef * errors[i+1][y] *\
@@ -63,26 +67,9 @@ class DigitsRecognitionNeuralNetwork:
 
     def check_sample(self, data):
         (image, target) = data
-        values = [numpy.divide(numpy.copy(image).flatten(), 15.0),
-                  numpy.zeros(32),
-                  numpy.zeros(20),
-                  numpy.zeros(10)]
-
-        for i in range(3):
-            values[i+1] = numpy.dot(values[i], self.weights[i])
-            values[i+1] = numpy.asarray([sigmoid(x) for x in values[i+1]])
-
-        errors = [numpy.zeros(64),
-                  numpy.zeros(32),
-                  numpy.zeros(20),
-                  numpy.zeros(10)]
-
-        errors[3] = -values[3]
-        errors[3][target] += 1
-
-        result = numpy.argmax(values[3])
-
-        return result == target, numpy.sum(errors[3]**2)
+        values, errors_output_layer = self.calculate_values_and_errors_output_layer(image, target)
+        result = numpy.argmax(values[-1])
+        return result == target, numpy.sum(errors_output_layer**2)
 
     def train(self, dataset, learning_density, learning_coef):
         actual_dataset = [(image, target) for image, target in zip(dataset.images, dataset.target)
@@ -104,16 +91,21 @@ class DigitsRecognitionNeuralNetwork:
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Digits Recognition Neural Network")
+    parser.add_argument("--iterations", type=int, default=30, help="NN learning iterations")
+    args = parser.parse_args()
+
     digits_dataset = sklearndatasets.load_digits()
     network = DigitsRecognitionNeuralNetwork()
 
     all_errors, all_pass_rates = [], []
-    for iteration in range(30):
+    total_iterations = args.iterations
+    for iteration in range(total_iterations):
         error, pass_rate = network.check(digits_dataset)
         print("Iteration {0}: pass rate {1}, error {2}".format(iteration+1, pass_rate, error))
         all_errors.append(error)
         all_pass_rates.append(pass_rate)
-        network.train(digits_dataset, 0.3, 0.1*(1-iteration/30))
+        network.train(digits_dataset, 0.3, 0.1 * (1 - iteration/total_iterations))
 
     plt.figure(1)
     plt.plot(all_errors)

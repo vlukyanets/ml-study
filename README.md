ffa2cdf980772f576131c52883225c2d2d71d3fc
Valentin Lukyanets <valikluks95@gmail.com>
2020-08-20 16:49:25 +0300

    Added save/load network to/from JSON file
    - Added classes for save/load in JSON format
    - Added command-line parameters
    - Fixed README.md

--- README.md
+++ README.md
@@ -5,15 +5,17 @@ Required Python 3.x to launch
 ## Digits Recognition (manual)
 
 Manual implementation of neural network that recognize digits from images 8x8 pixels.
-Image is 8x8 array with values [0..15] representing gray level of pixel.
+Image is 8x8 array with values `[0..15]` representing gray level of pixel.
 
 Launch:
 ```
-python3 digits_recognition.py [--iterations X] [--hide-plots]
+python3 digits_recognition.py [--iterations X] [--hide-plots] [--nn-save-to FILE] [--nn-load-from FILE]
 ```
 
 Parameters:
 ```
---iterations X - set how much iterations of training/checking should be performed (default 30)
---hide-plots   - do not show figures with pass rate and sum of square error on check (default: show)
+--iterations X      - set how much iterations of training/checking should be performed (default 30)
+--hide-plots        - do not show figures with pass rate and sum of square error on check (default: show)
+--nn-save-to FILE   - if specified, save network after training to text file
+--nn-load-from FILE - if specified, load network from text file instead of training
 ```
--- digits_recognition.py
+++ digits_recognition.py
@@ -3,6 +3,7 @@ import numpy
 import random
 import matplotlib.pyplot as plt
 import argparse
+import json
 
 
 def sigmoid(x: float):
@@ -62,8 +63,6 @@ class DigitsRecognitionNeuralNetwork:
                     self.weights[i][x][y] += learning_coef * errors[i+1][y] *\
                                              bounds(derivative(values[i+1][y]), 0.0001, 0.9999) * values[i][x]
 
-        # print(self.weights)
-
     def check_sample(self, data):
         (image, target) = data
         values, errors_output_layer = self.calculate_values_and_errors_output_layer(image, target)
@@ -89,30 +88,72 @@ class DigitsRecognitionNeuralNetwork:
         return error, passed / total
 
 
+class DigitsRecognitionNeuralNetworkSaver:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def save(self, network):
+        data = dict(layer_sizes=network.layer_sizes,
+                    weights=[weights_layer.tolist() for weights_layer in network.weights])
+        with open(self.filename, "w") as f:
+            json.dump(data, fp=f)
+
+
+class DigitsRecognitionNeuralNetworkLoader:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def load(self):
+        with open(self.filename, "r") as f:
+            data = json.load(fp=f)
+
+        if not data:
+            return None
+
+        network = DigitsRecognitionNeuralNetwork(data["layer_sizes"])
+        network.weights = [numpy.asarray(weights_layer) for weights_layer in data["weights"]]
+        return network
+
+
 def main():
     parser = argparse.ArgumentParser(description="Digits Recognition Neural Network")
     parser.add_argument("--iterations", type=int, default=30, help="NN learning iterations")
     parser.add_argument("--hide-plots", action='store_true', help="Do not show figures with ML stats")
+    parser.add_argument("--nn-save-to", type=str, default=None,
+                        help="If specified, save network after training to text file")
+    parser.add_argument("--nn-load-from", type=str, default=None,
+                        help="If specified, load network from text file instead of training")
     args = parser.parse_args()
 
     digits_dataset = sklearndatasets.load_digits()
-    network = DigitsRecognitionNeuralNetwork([64, 32, 20, 10])
-
-    all_errors, all_pass_rates = [], []
-    total_iterations = args.iterations
-    for iteration in range(total_iterations):
+    load_from_filename = args.nn_load_from
+    if load_from_filename:
+        loader = DigitsRecognitionNeuralNetworkLoader(load_from_filename)
+        network = loader.load()
         error, pass_rate = network.check(digits_dataset)
-        print("Iteration {0}: pass rate {1}, error {2}".format(iteration+1, pass_rate, error))
-        all_errors.append(error)
-        all_pass_rates.append(pass_rate)
-        network.train(digits_dataset, 0.3, 0.1 * (1 - iteration/total_iterations))
+        print("Pass rate {0}, error {1}".format(pass_rate, error))
+    else:
+        network = DigitsRecognitionNeuralNetwork([64, 32, 20, 10])
+        all_errors, all_pass_rates = [], []
+        total_iterations = args.iterations
+        for iteration in range(total_iterations):
+            error, pass_rate = network.check(digits_dataset)
+            print("Iteration {0}: pass rate {1}, error {2}".format(iteration+1, pass_rate, error))
+            all_errors.append(error)
+            all_pass_rates.append(pass_rate)
+            network.train(digits_dataset, 0.3, 0.1 * (1 - iteration/total_iterations))
 
-    if not args.hide_plots:
-        plt.figure(1)
-        plt.plot(all_errors)
-        plt.figure(2)
-        plt.plot(all_pass_rates)
-        plt.show()
+        if not args.hide_plots:
+            plt.figure(1)
+            plt.plot(all_errors)
+            plt.figure(2)
+            plt.plot(all_pass_rates)
+            plt.show()
+
+        save_to_filename = args.nn_save_to
+        if save_to_filename:
+            saver = DigitsRecognitionNeuralNetworkSaver(save_to_filename)
+            saver.save(network)
 
 
 if __name__ == "__main__":
